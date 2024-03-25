39b86891225075c2ff8f0df0114b6629c30afb7f
shoaib665583 <shoaib@cablesmith.in>
2024-03-25 20:15:23 +0530

    components exported from index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import DeleteMessage from './components/DeleteMessage/DeleteMessage'
 import ErrorMessage from './components/ErrorMessage/ErrorMessage'
 import SuccessMessage from './components/SuccessMessage/SuccessMessage'
 import WarningMessage from './components/WarningMessage/WarningMessage'
 
-ReactDOM.render(
-    <React.StrictMode>
-        <DeleteMessage />
-        <ErrorMessage />
-        <SuccessMessage />
-        <WarningMessage />
-
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+export { DeleteMessage, ErrorMessage, SuccessMessage, WarningMessage }
